aba1abe70a962982b1f956ba8b7c8138cb2c52b1
Saibaba-1610 <sai1610k@gmail.com>
2024-06-17 10:29:42 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 
 const userRoutes = require('./routes/userRoutes')
 
 //express app
 const app = express()
 
+app.use(cors())
 //middleware
 app.use(express.json())
 app.use((req, res, next) => {
